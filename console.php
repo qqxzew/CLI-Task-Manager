5e595dedd3b5f5c815ea49922f9e410708130b93
Andrii Fedorov <andrii.fedorov@infis.cz>
2026-05-12 08:39:21 +0200

    console.php

---
+++ console.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'src/task.php';
+use TaskManager\Tasks;
+
+$setStorage = new Tasks();
+$setStorage->initializeStorage();
--- src/task.php
+++ src/task.php
@@ -2,17 +2,14 @@
 namespace TaskManager;
 
     
-const PATH_TO_STORAGE = '../data/tasks.json';
+const PATH_TO_STORAGE = 'data/tasks.json';
 
 class Tasks{
     function initializeStorage(){
     
         if(!file_exists(PATH_TO_STORAGE)){
-            mkdir('../data', 0777, true);
+            mkdir('data', 0777, true);
             file_put_contents(PATH_TO_STORAGE, json_encode([]));
         }
     }
 }
-
-$setStorage = new Tasks();
-$setStorage->initializeStorage();
